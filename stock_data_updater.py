e3d7cee4698ec79643da2d419ac41148b15c5681
Ubuntu <ubuntu@ip-172-31-25-51.ec2.internal>
2023-03-15 12:15:55 +0000

    loading streaming data to tables

--- stock_data_updater.py
+++ stock_data_updater.py
@@ -1,14 +1,27 @@
 import yfinance as yf
 import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
+
 ticker = ['SPY', 'AAPL']#, 'UBER', 'NFLX', 'AMZN', 'GOOGL','QQQ','TSLA','META','MSFT','QYLD', 'XOM','GE','DIS']
 columns = ['Ticker', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
 df_all = pd.DataFrame(columns=columns)
 for t in ticker:
     data = yf.download(tickers=t,  # list of tickers
-                       period="5m",  # time period
+                       period="1m",  # time period
                        interval="1m",  # trading interval
                        prepost=False)  # download pre/post market hours data?
-    data['Ticker']=t
-    df_all = pd.concat([df_all, data])
-    print(df_all.head())
-df_all.to_csv('./data/stock_data.csv', mode='a')
+    print(data)
+    data.drop(columns=['Adj Close'],axis=1, inplace=True)
+    data.reset_index(inplace=True)
+                    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(
+        host="localhost",
+        database="stock_data",
+        user="postgres",
+        password="postgres"
+    )
+    engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/stock_data')
+
+    data.to_sql(f"{t}", con=engine, schema="raw_stocks",if_exists="append", index=False)
+    print(data)
